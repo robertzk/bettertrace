a422f79ae3f7adae333c45328c171f6432fdd2bb
RobertZK <technoguyrob@gmail.com>
2015-04-03 13:59:37 -0500

    magic_dump helper for debugging in your debugger

--- tests/testthat/test-stacktrace.R
+++ tests/testthat/test-stacktrace.R
@@ -5,4 +5,15 @@ with_options(error = stacktrace, {
   test_that("it does not print a stacktrace on a simple error", {
     expect_output(try(silent = TRUE, foo()), "")
   })
+
+  test_that("it prints a nice stacktrace on a more involved error", {
+    within_file_structure(list("foo.R" = "
+      foo <- function() { bar() + 1 }
+      bar <- function() { baz() + 1 }
+      bar() + 1
+    "), {
+      debugonce(stacktrace)
+      source(file.path(tempdir, "foo.R"))
+    })
+  })
 })
