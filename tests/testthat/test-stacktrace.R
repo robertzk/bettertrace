e37ca227d55eaa4e896320984d6df8b9a49aca0f
RobertZK <technoguyrob@gmail.com>
2015-04-03 13:43:06 -0500

    simple stacktrace test

--- tests/testthat/test-stacktrace.R
+++ tests/testthat/test-stacktrace.R
@@ -1,6 +1,8 @@
 context("stacktrace")
 library(testthatsomemore)
 
-test_that("it does not print a stacktrace on a simple error", {
-  expect_output(try(silent = TRUE, 
+with_options(error = stacktrace, { 
+  test_that("it does not print a stacktrace on a simple error", {
+    expect_output(try(silent = TRUE, foo()), "")
+  })
 })
