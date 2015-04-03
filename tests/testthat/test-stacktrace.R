a0795ad1ee5761bcb9084c869c824d9fc782c0ba
RobertZK <technoguyrob@gmail.com>
2015-04-03 14:54:24 -0500

    check the same test with srcref mocking

--- tests/testthat/test-stacktrace.R
+++ tests/testthat/test-stacktrace.R
@@ -40,6 +40,9 @@ with_options(error = stacktrace, {
       package_stub("base", "geterrmessage", function(...) "Error: errmsgyo",
         expect_match(eval_in(foo()), "Error: errmsgyo")
       )
+      package_stub("base", "geterrmessage", function(...) "Error: errmsgyo\033",
+        expect_match(eval_in(foo()), "Error: errmsgyo")
+      )
     })
   })
 
@@ -50,6 +53,23 @@ with_options(error = stacktrace, {
       bar <- function() { baz() + 1 }
     "), {
       not(expect_match)(eval_in(foo(), keep.source = FALSE), "foo\\.R")
+    })
+  })
+
+  test_that("it cannot match filenames if the srcref has no file (mocked)", {
+    on.exit(rm(list = c("foo", "bar"), envir = .GlobalEnv), add = TRUE)
+    within_file_structure(list("foo.R" = "
+      foo <- function() { bar() + 1 }
+      bar <- function() { baz() + 1 }
+    "), {
+      package_stub("base", "attr", function(x, which) {
+        out <- .Primitive("attr")(x, which)
+        if ("filename" %in% names(out) && nzchar(out$filename)) 
+          out$filename <- ""
+        out
+      }, 
+        not(expect_match)(eval_in(foo()), "foo\\.R")
+      )
     })
   })
 })
